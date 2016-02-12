1df124bad4bd2e7de70057863031d1ecefc7b6a6
Shivendra Srivastava <shivendra.srivastava@razorfish.com>
2016-02-12 16:54:20 -0600

    Added messages and checks for file permission script

--- pyjira/init.py
+++ pyjira/init.py
@@ -1,9 +1,22 @@
 import os
 import stat
 
+
 st = os.stat('extract.py')
-os.chmod('extract.py', st.st_mode | stat.S_IEXEC)
+if not bool(st.st_mode & stat.S_IEXEC):
+	print "Setting permissions for extract.py"
+	os.chmod('extract.py', st.st_mode | stat.S_IEXEC)
+else:
+	print "  "
+	print "---------------------Permissions assigned for extract.py successfully--------------------"
+	print "  "
 
 st = os.stat('run.py')
-os.chmod('run.py', st.st_mode | stat.S_IEXEC)
+if not bool(st.st_mode & stat.S_IEXEC):
+	print "Setting permissions for run.py"
+	os.chmod('run.py', st.st_mode | stat.S_IEXEC)
+else:
+	print "  "
+	print "---------------------Permissions assigned for run.py successfully------------------------"
+	print "  "
 
